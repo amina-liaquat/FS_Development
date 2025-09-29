1a5abd96ac91acf6272887968281b238546f0079
Amina Liaquat <aminaliaquat41@gmail.com>
2025-09-29 10:45:13 +0500

    Create operators.js

---
+++ Week 2 /Day 1 & 2 /operators.js
@@ -0,0 +1,70 @@
+// Operators in JavaScript
+
+   // Arithmetic Operators
+
+let a = 10, b = 3;
+console.log(a + b); // 13
+console.log(a - b); // 7
+console.log(a * b); // 30
+console.log(a / b); // 3.333...
+console.log(a % b); // 1 (remainder)
+console.log(a ** b); // 1000 (power)
+
+      // Assignment Operators
+
+
+let x = 5;
+x += 3;  // x = x + 3 → 8
+x -= 2;  // x = x - 2 → 6
+x *= 4;  // x = x * 4 → 24
+x /= 2;  // x = x / 2 → 12
+
+
+       // Comparison Operators
+
+
+console.log(5 == "5");   // true (only checks value)
+console.log(5 === "5");  // false (checks value + type)
+console.log(5 != "5");   // false
+console.log(5 !== "5");  // true
+console.log(10 > 5);     // true
+console.log(10 <= 5);    // false
+
+
+       // Logical Operators
+
+
+console.log(true && false); // false (AND)
+console.log(true || false); // true  (OR)
+console.log(!true);         // false (NOT)
+
+
+    // increment & Decrement
+
+let n = 5;
+console.log(++n); // 6 (pre-increment)
+console.log(n++); // 6 (then increases to 7)
+
+
+      // Ternary Operator
+
+
+let age = 18;
+let canVote = (age >= 18) ? "Yes" : "No";
+console.log(canVote); // Yes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
