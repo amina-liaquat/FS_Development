a8a48afc94f22d06e5d14fe5158bf7528f613d80
Amina Liaquat <aminaliaquat41@gmail.com>
2025-09-29 10:41:10 +0500

    Create datatypes.js

---
+++ Week 2 /Day 1 & 2 /datatypes.js
@@ -0,0 +1,48 @@
+// Data Types in JavaScript
+
+   // Primitive Data Types
+
+  // string, num , boolean, undefined, null,symbol,biglnt
+
+let str = "Hello";
+
+let num = 42;
+let pi = 3.14;
+
+let isOnline = false;
+
+let x;
+console.log(x); // undefined
+
+let y = null;
+
+let id = Symbol("id");
+
+let bigNum = 123456789012345678901234567890n;
+
+    // Non-Primitive Data Types
+
+   // object, Array , Function 
+
+let person = { name: "Amina", age: 22 };
+
+let fruits = ["apple", "banana", "mango"];
+
+function greet() { return "Hello"; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
