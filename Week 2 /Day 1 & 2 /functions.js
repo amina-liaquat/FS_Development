f6ef74256318443b877ab2843ff51145c1b2bc63
Amina Liaquat <aminaliaquat41@gmail.com>
2025-09-30 10:52:32 +0500

    Create functions.js

---
+++ Week 2 /Day 1 & 2 /functions.js
@@ -0,0 +1,27 @@
+  // Functions 
+
+function greet() {
+  console.log("Hello, world!");
+}
+
+// Call the function
+greet();  // Output: Hello, world!
+
+
+     // Functions types in JS 
+
+   // Function Declaration
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(5, 3)); // 8
+
+    // Function Expression
+
+const multiply = function(x, y) {
+  return x * y;
+};
+
+console.log(multiply(4, 6)); // 24
