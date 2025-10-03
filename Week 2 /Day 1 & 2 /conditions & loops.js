3c70e4e910590c28d846623f0404fb9e4aee7230
Amina Liaquat <aminaliaquat41@gmail.com>
2025-10-03 11:35:57 +0500

    Create conditions & loops.js

---
+++ Week 2 /Day 1 & 2 /conditions & loops.js
@@ -0,0 +1,18 @@
+// if-else
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+// for loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Number:", i);
+}
+
+// while loop
+let count = 0;
+while (count < 3) {
+    console.log("Hello", count);
+    count++;
+}
