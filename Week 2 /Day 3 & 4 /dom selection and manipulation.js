7e730fe129d6742903e4c395b0167586bef9496c
Amina Liaquat <aminaliaquat41@gmail.com>
2025-10-03 11:38:16 +0500

    Update and rename Week 2 /Day 3 & 4 to Week 2 /Day 3 & 4 /dom selection and manipulation.js

---
+++ Week 2 /Day 3 & 4 /dom selection and manipulation.js
@@ -0,0 +1,13 @@
+   // index.html
+// <body>
+//   <h1 id="title">Hello JS</h1>
+//   <button id="btn">Click Me</button>
+//   <input type="text" id="input" placeholder="Type something">
+// </body>
+
+let title = document.getElementById("title");
+title.innerText = "Welcome to JavaScript!";
+title.style.color = "purple";
+
+
+   
