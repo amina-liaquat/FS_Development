7a2cc3f9d58ecd093050148a851676572ad498a5
Amina Liaquat <aminaliaquat41@gmail.com>
2025-10-05 15:43:21 +0500

    Create async & await.js

---
+++ Week 2 /Day 05 /async & await.js
@@ -0,0 +1,10 @@
+async function fetchData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+}
+fetchData();
